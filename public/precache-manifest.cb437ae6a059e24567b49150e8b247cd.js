5f181a53b8a731824287a469aaf35a6c27226a68
gcho <ghcho20@gmail.com>
2022-12-06 07:35:23 +0900

    init project original source by MongoDB:   https://drive.google.com/drive/folders/1OEbVvosmSpAZdP6wxpc5Zh1GWQ1S035k?usp=sharing

---
+++ public/precache-manifest.cb437ae6a059e24567b49150e8b247cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2932de5995853a8b15fc",
+    "url": "/static/css/main.4ffd5486.chunk.css"
+  },
+  {
+    "revision": "2932de5995853a8b15fc",
+    "url": "/static/js/main.2932de59.chunk.js"
+  },
+  {
+    "revision": "621a5e6308c1ac305775",
+    "url": "/static/js/1.621a5e63.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "049cb846e51440f829d77c182bfb671b",
+    "url": "/static/media/mongodb-logo.049cb846.png"
+  },
+  {
+    "revision": "64b32773ca6e0209868eb2a7afe1070b",
+    "url": "/index.html"
+  }
+];
---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React, { Component }  from 'react';
+import './app.css';
+import { Home } from './pages/Home/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+export default function app() {
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/index.html" element={<Home />} />
+                <Route path="/index" element={<Home />} />
+            </Routes>
+        </Router>
+    );
+}
